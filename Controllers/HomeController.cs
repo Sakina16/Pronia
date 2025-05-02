b9e03c580ce132a5390f03cd3da7d6890892670a
SaraHemidova <aytanhuseynli24>
2025-05-02 13:29:30 +0400

    added task

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace Pronio.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
